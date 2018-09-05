4c3633f97ac96b5b987b7523c91706da1396dbca
SMK TELKOM <SMK TELKOM@192.168.56.1>
2018-09-05 20:33:25 +0700

    commit keseluruhan

---
+++ src/penawaran_jobsheet3/tes_sekolah.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penawaran_jobsheet3;
+
+/**
+ *
+ * @author SMK TELKOM
+ */
+public class tes_sekolah {
+     public static void main (String [] args)
+    {
+    perpustakaan obj = new perpustakaan(4,"daun yang jatuh tidak membenci angin","bahasa indonesia",11,"bu felly");
+    obj.tampilkan();
+}
+}
